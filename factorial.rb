264ec78a0a3d023e450feb89a785e9f067419e19
=Rovin Varshney <=rovin100@gmail.com>
2013-12-24 15:52:47 +0530

    basic programming in ruby

---
+++ factorial.rb
@@ -0,0 +1,11 @@
+
+ p "Enter a number to calculate it's factorial:"
+  num = gets.to_i
+  c = 1
+  fact = 1
+  
+  while(c <= num)
+    fact = fact * c
+    c = c+1
+  end
+  p "Factorial of #{num} is #{fact}" 
---
+++ febbonoci_series.rb
@@ -0,0 +1,20 @@
+#Febonocci series in ruby
+p 'Enter the number upto which you want to print febonocci series : '
+num = gets.to_i
+c = 0
+first = 0
+second = 1
+val = 0
+while(c < num )
+  if  c <= 1 
+    val = c
+  else
+    val = first + second
+    first = second
+    second = val
+  end
+ p val
+ c = c+1
+end
+
+
